3b1fd669760d2c0dfd2de164729a59202fcc22c4
Đức Bùi <117875129+baemgao@users.noreply.github.com>
2023-12-23 15:05:54 +0700

    [3mallrice][Splash Screen]

--- lib/representation/splash_screen.dart
+++ lib/representation/splash_screen.dart
@@ -3,7 +3,7 @@ import 'package:exe201_lumos_mobile/core/helper/image_helper.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen ({Key? key}) : super(key : key);
+  const SplashScreen({Key? key}) : super(key: key);
 
   static String routeName = '/splash_screen';
 
@@ -17,7 +17,11 @@ class _SplashScreenState extends State<SplashScreen> {
     return Stack(
       // widget nào được viết trước sẽ nằm phía dưới còn widget nào viết sau sẽ nằm phía trên
       children: [
-        ImageHelper.loadFormAsset(AssetHelper.imgSplashBg, fit: BoxFit.fitWidth)
+        Positioned.fill(
+            child: ImageHelper.loadFormAsset(AssetHelper.imgSplashBg,
+                fit: BoxFit.fitWidth)),
+        Positioned.fill(
+            child: ImageHelper.loadFormAsset(AssetHelper.imgLogo)),
       ],
     );
   }
