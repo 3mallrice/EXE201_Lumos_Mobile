550f4b78bd1fc4a48519174fb79a55bafca9884e
Maotou <93722462+maotou-spy@users.noreply.github.com>
2024-01-09 10:47:01 +0700

    [maotou][update member home screen]

--- lib/representation/member/home_screen.dart
+++ lib/representation/member/home_screen.dart
@@ -18,46 +18,45 @@ class _MemberHomeState extends State<MemberHome> {
     double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-        appBar: const AppBarCom(
-          appBarText: 'Home',
-        ),
-        body: Container(
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 0,
-                  width: screenWidth,
-                  child: ImageHelper.loadFormAsset(AssetHelper.memberBanner,
-                      fit: BoxFit.fill),
-                ),
-                const Positioned(
-                    top: 50.0,
-                    left: 30.0,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Medical At Home',
-                          style: TextStyle(
-                            fontFamily: 'poppins',
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700,
-                            color: Color(0xFF0A4F45),
-                          ),
-                        ),
-                        Text(
-                          'Booking',
-                          style: TextStyle(
-                            fontFamily: 'poppins',
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700,
-                            color: Color(0xFF0A4F45),
-                          ),
-                        ),
-                      ],
-                    )),
-              ],
-            ),
-            Stack));
+      appBar: const AppBarCom(
+        appBarText: 'Home',
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            width: screenWidth,
+            child: ImageHelper.loadFormAsset(AssetHelper.memberBanner,
+                fit: BoxFit.fill),
+          ),
+          const Positioned(
+              top: 50.0,
+              left: 30.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Medical At Home',
+                    style: TextStyle(
+                      fontFamily: 'poppins',
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFF0A4F45),
+                    ),
+                  ),
+                  Text(
+                    'Booking',
+                    style: TextStyle(
+                      fontFamily: 'poppins',
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFF0A4F45),
+                    ),
+                  ),
+                ],
+              )),
+        ],
+      ),
+    );
   }
 }
