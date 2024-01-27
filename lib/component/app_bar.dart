a19e5221064b57c076d1c436e679aabf3b3cf2ef
Maotou <93722462+maotou-spy@users.noreply.github.com>
2024-01-27 12:20:35 +0700

    [maotou][fix appbar]

--- lib/component/app_bar.dart
+++ lib/component/app_bar.dart
@@ -1,5 +1,4 @@
 import 'package:exe201_lumos_mobile/representation/member/member_home.dart';
-import 'package:exe201_lumos_mobile/representation/share/account_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../../core/const/color_const.dart';
