915ed10f5a1fec2d3bc018e674f7df6fabe371b5
Đức Bùi <bhduc264@gmail.com>
2024-01-27 14:28:29 +0700

    [3mallrice][medical report detail]

--- lib/representation/member/medical_report_detail.dart
+++ lib/representation/member/medical_report_detail.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:exe201_lumos_mobile/component/app_bar.dart';
+import 'package:exe201_lumos_mobile/core/const/color_const.dart';
 import 'package:flutter/material.dart';
 
 class MedicalReportDetail extends StatelessWidget {
@@ -8,12 +11,191 @@ class MedicalReportDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AppBarCom(
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: const AppBarCom(
         leading: true,
         appBarText: "Hồ sơ bệnh nhân",
       ),
-      body: Column(),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: SingleChildScrollView(
+          child: Container(
+            width: screenWidth * 0.9,
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Họ và tên: ',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Raleway',
+                        color: ColorPalette.blueBold2,
+                      ),
+                    ),
+                    Text(
+                      'Nguyễn Văn A',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Raleway',
+                        color: ColorPalette.blueBold2,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Danh xưng: ',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Raleway',
+                            color: ColorPalette.blueBold2,
+                          ),
+                        ),
+                        Text(
+                          'Anh',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                            fontFamily: 'Raleway',
+                            color: ColorPalette.blueBold2,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Giới tính: ',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Raleway',
+                            color: ColorPalette.blueBold2,
+                          ),
+                        ),
+                        Text(
+                          'Nam',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                            fontFamily: 'Raleway',
+                            color: ColorPalette.blueBold2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Ngày sinh: ',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Raleway',
+                        color: ColorPalette.blueBold2,
+                      ),
+                    ),
+                    Text(
+                      '19/05/1989',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                        fontFamily: 'Poppins',
+                        color: ColorPalette.blueBold2,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Nhóm máu: ',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Raleway',
+                        color: ColorPalette.blueBold2,
+                      ),
+                    ),
+                    Text(
+                      'B',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                        fontFamily: 'Raleway',
+                        color: ColorPalette.blueBold2,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Số điện thoại: ',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Raleway',
+                        color: ColorPalette.blueBold2,
+                      ),
+                    ),
+                    Text(
+                      '0934853348',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                        fontFamily: 'Poppins',
+                        color: ColorPalette.blueBold2,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Ghi chú: ',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Raleway',
+                        color: ColorPalette.blueBold2,
+                      ),
+                    ),
+                    Text(
+                      'Hoàn toàn bình thường, khoẻ mạnh',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                        fontFamily: 'Raleway',
+                        color: ColorPalette.blueBold2,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
