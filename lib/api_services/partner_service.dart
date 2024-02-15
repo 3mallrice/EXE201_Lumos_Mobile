3178733d16b8290ec99f103a667ef5d01af5b741
Maotou <93722462+maotou-spy@users.noreply.github.com>
2024-02-15 15:59:37 +0700

    [maotou][config api request]

---
+++ lib/api_services/partner_service.dart
@@ -0,0 +1,12 @@
+// ignore: unused_import
+import 'package:http/http.dart' as http;
+
+import 'api_service.dart';
+
+class CallPartnerApi {
+  static const apiName = 'partner';
+  static const rootApi = ApiService.rootApi;
+  final String api = rootApi + apiName;
+  // ignore: unused_field
+  final String _imgUrl = '';
+}
--- lib/apiservices/api_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:http/http.dart' as http;
-
-class CallApi {
-  final String _url = '';
-  final String _imgUrl = '';
-  String token = "";
-}
