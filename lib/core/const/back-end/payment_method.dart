fd1d4026727ad8ea6d6dc8f8a2feaf80b267194e
Maotou <93722462+maotou-spy@users.noreply.github.com>
2024-02-28 02:01:52 +0700

    [amotou][update payment method + workshift]

---
+++ lib/core/const/back-end/payment_method.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: constant_identifier_names
+
+class PaymentMedthod {
+  static const int COD = 1;
+  static const int MOMO = 2;
+  static const int VISA = 3;
+  static const int MASTER_CARD = 4;
+  static const int BANK_TRANSFER = 5;
+}
