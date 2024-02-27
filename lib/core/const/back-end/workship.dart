7555c756c4a032c2963cf406fdb350f3fe3ee602
Maotou <93722462+maotou-spy@users.noreply.github.com>
2024-02-28 03:19:18 +0700

    [maotou][add workshift and dayofweek]

--- lib/core/const/back-end/workship.dart
+++ lib/core/const/back-end/workship.dart
@@ -1,10 +1,27 @@
-final class Workshift {
-  final int id;
-  final String name;
+// ignore_for_file: constant_identifier_names
 
-  const Workshift(this.id, this.name);
+class Moring {
+  static const int id = 1;
+  static const String name = '06:00 - 12:00';
+}
 
-  static const Workshift morning = Workshift(1, '06:00 - 12:00');
-  static const Workshift afternoon = Workshift(2, '12:00 - 18:00');
-  static const Workshift night = Workshift(3, '18:00 - 22:00');
+class Afternoon {
+  static const int id = 2;
+  static const String name = '12:00 - 18:00';
+}
+
+class Night {
+  static const int id = 3;
+  static const String name = '18:00 - 22:00';
+}
+
+class WorkshiftTime {
+  // Morning: 06:00 - 12:00
+  static const String Morning = '06:00 - 12:00';
+
+  // Afternoon: 12:00 - 18:00
+  static const String Afternoon = '12:00 - 18:00';
+
+  // Night: 18:00 - 22:00
+  static const String Night = '18:00 - 22:00';
 }
